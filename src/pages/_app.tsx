33490f32d251b433c3545c6a1d433e08bf776827
namminimi <tbvhdaos@naver.com>
2024-03-16 00:01:01 +0900

    fix: 트위터 meta 태그 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,7 +18,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property='og:url' content={process.env.NEXT_PUBLIC_BASE_URL} />
         {/*<!-- 트위터 --> */}
         <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:image' content='/avifs/portfolio-logo.avif' />
+        <meta
+          name='twitter:image'
+          content={`${process.env.NEXT_PUBLIC_BASE_URL}avifs/portfolio-logo.avif`}
+        />
         <meta name='twitter:title' content="Nam Minseop's Portfolio" />
         <meta name='twitter:description' content='남민섭의 포트폴리오입니다' />
       </Head>
