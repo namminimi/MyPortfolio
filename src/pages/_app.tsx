2e5d73c0f60767d387c04809eeacb00d6b20bd69
namminimi <tbvhdaos@naver.com>
2024-03-17 00:26:26 +0900

    feat: GA 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
+import { GA_TRACKING_ID } from '@/libs/gtag';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -25,6 +27,26 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name='twitter:title' content="Nam Minseop's Portfolio" />
         <meta name='twitter:description' content='남민섭의 포트폴리오입니다' />
       </Head>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        async
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy='afterInteractive'
+        id='google-analytics-script'
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('ts', new Date());
+              gtag('config', '${GA_TRACKING_ID}', { 
+                page_path: window.location.pathname,
+              });
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
